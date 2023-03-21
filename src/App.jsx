5ed0a615c551756f3f7656e2f9abb90981ad6b35
hussain-shariff <hussainowaiz070@gmail.com>
2023-03-21 20:46:48 +0530

    gradient landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@
 function App() {
 
   return (
-    <div className="App">
-       <h1 className="text-3xl font-bold underline">
-          Hello world!
-      </h1>
+    <div className="bg-[#12111d] h-screen relative overflow-hidden" >
+      <div className="absolute -top-10 -right-10 w-96 h-72 bg-[#2a3f52] blur-3xl opacity-70"></div>
+      <div className="absolute -bottom-10 -left-10 w-96 h-72 bg-[#36104b] blur-3xl opacity-70"></div>
     </div>
   )
 }
