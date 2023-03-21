278aba06e6def93eac4d21f136474d2b1c8abe5e
hussain-shariff <hussainowaiz070@gmail.com>
2023-03-21 21:19:21 +0530

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Logo from "./components/Logo"
 
 function App() {
 
@@ -5,6 +6,7 @@ function App() {
     <div className="bg-[#12111d] h-screen relative overflow-hidden" >
       <div className="absolute -top-10 -right-10 w-96 h-72 bg-[#2a3f52] blur-3xl opacity-70"></div>
       <div className="absolute -bottom-10 -left-10 w-96 h-72 bg-[#36104b] blur-3xl opacity-70"></div>
+      <Logo/>
     </div>
   )
 }
---
+++ src/components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div className='flex text-white items-center justify-evenly cursor-pointer w-28'>
+        <div className='w-10 h-10 bg-gradient-to-r from-purple-600 rounded-xl' >
+            <h1 className='text-center font-bold text-3xl'>J</h1>
+        </div>
+        <p className='font-semibold text-xl'>jobster</p>
+    </div>
+    
+  )
+}
+
+export default Logo
