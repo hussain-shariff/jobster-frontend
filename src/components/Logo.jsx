ac422a7cd98910b0274f81caef520ce3f9cbe97f
hussain-shariff <hussainowaiz070@gmail.com>
2023-03-24 16:45:55 +0530

    test

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo() {
   return (
     <div className='flex text-white items-center justify-between w-36 cursor-pointer'>
-        <div className='w-10 h-10 bg-gradient-to-r from-pink-700 rounded-xl' >
+        <div className='w-10 h-10 bg-gradient-to-r from-purple-600 rounded-xl' >
             <h1 className='text-center font-bold text-3xl'>J</h1>
         </div>
         <p className='font-semibold text-3xl mb-1'>jobster</p>
