1d5176a6a87d672df5599df8704e112dcb467d76
hussain-shariff <hussainowaiz070@gmail.com>
2023-03-25 19:56:01 +0530

    area chart

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import StatsCard from '../components/StatsCard'
 import {
     faSuitcaseRolling,
@@ -6,8 +6,10 @@ import {
     faBug
     } from '@fortawesome/free-solid-svg-icons'
 import BarChartComp from '../components/BarChartComp'
+import AreaChartComp from '../components/AreaChartComp'
 
 function Stats() {
+  const [showBarChart, setshowBarChart] = useState(true)
   return (
     <div>
       <div className='grid grid-cols-1 gap-6 px-6 md:grid-cols-3 md:px-10 my-10'>
@@ -30,8 +32,14 @@ function Stats() {
       </div>
       <div className='mx-auto mt-10 md:w-3/4 flex flex-col items-center'>
         <h1 className='text-center text-2xl text-white md:text-3xl font-semibold'>Monthly Applications</h1>
-        <button className='text-[#8884d8] w-32 cursor-pointer mt-3 mb-7 text-xl font-semibold'>Area Chart</button>
-        <BarChartComp />
+        <div className='text-[#8884d8] group flex w-32 cursor-pointer mt-3 mb-7 text-xl font-semibold'
+        onClick={()=> setshowBarChart(!showBarChart)}>
+          {showBarChart?  'Area Chart' : "Bar Chart" } 
+          <h1 className='group-hover:translate-x-2
+          transition ease-out duration-300 ml-1'>→</h1>
+        </div>
+        {showBarChart && <BarChartComp />}
+        {!showBarChart && <AreaChartComp/>}
       </div>
     </div>
   )
