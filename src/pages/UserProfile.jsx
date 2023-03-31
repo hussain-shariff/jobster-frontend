9274447e5d72d1771eb95adfd26c6190e586862e
hussain-shariff <hussainowaiz070@gmail.com>
2023-03-31 23:04:14 +0530

    profile header

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -17,27 +17,30 @@ function UserProfile() {
     }
 
   return (
-    <form className='grid grid-cols-1 md:grid-cols-3 gap-3 bg-white/20 p-10' onSubmit={handleSubmit}>
-        <JobInput
-            name='updatedUser'
-            handleChange={handleSearch}
-            value={ updatedUser }
-            type="text"/>
-        <JobInput
-            name='lastname'
-            handleChange={handleSearch}
-            value={ lastname }
-            type="text"/>
-        <JobInput
-            name='userLocation'
-            handleChange={handleSearch}
-            value={ userLocation }
-            type="text"/>
-        <button type='submit' className='bg-white/30  rounded-md w-36 hover:bg-white/40
-            transition ease-out duration-300 py-1 text-white'>
-            Save Changes
-        </button>
-    </form>
+    <div className='bg-white/20 p-10 rounded-md'>
+        <h1 className='text-center text-3xl text-white mb-3'>Profile</h1>
+        <form className='grid grid-cols-1 md:grid-cols-3 gap-3 ' onSubmit={handleSubmit}>
+            <JobInput
+                name='updatedUser'
+                handleChange={handleSearch}
+                value={ updatedUser }
+                type="text"/>
+            <JobInput
+                name='lastname'
+                handleChange={handleSearch}
+                value={ lastname }
+                type="text"/>
+            <JobInput
+                name='userLocation'
+                handleChange={handleSearch}
+                value={ userLocation }
+                type="text"/>
+            <button type='submit' className='bg-white/30  rounded-md w-36 hover:bg-white/40
+                transition ease-out duration-300 py-1 text-white'>
+                Save Changes
+            </button>
+        </form>
+    </div>
   )
 }
 
