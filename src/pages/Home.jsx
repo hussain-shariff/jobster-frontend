4585fe31224759bb3cbdb09761586aefe2bde9c9
hussain-shariff <hussainowaiz070@gmail.com>
2023-04-12 19:31:52 +0530

    herosection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,7 @@ import NavBar from '../components/NavBar'
 
 function Home() {
   return (
-    <div className="bg-[#12111d] h-screen relative overflow-hidden" > 
-        <div className='absolute top-36 right-36 w-[400px] h-[400px] rounded-full bg-[#1e3850] opacity-80 blur-3xl hidden md:block'></div>
+    <div className="bg-[#12111d] min-h-screen relative overflow-hidden" > 
         <header>
             <NavBar/>
         </header>
