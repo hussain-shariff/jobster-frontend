1b82f4290a789f7c5cc1bfb6c7d8601e7aea7e50
hussain-shariff <hussainowaiz070@gmail.com>
2025-07-04 20:19:50 +0530

    chore: made sign in page responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,22 +6,24 @@ function HeroSection() {
     <div className='text-white text-center md:text-start relative mt-20'>
         <div className='absolute -top-5 left-14 w-80 h-80 rounded-full bg-[#460d64] blur-3xl'></div>
         <div className='relative px-8 md:px-0 flex flex-col md:flex-row gap-10 items-center justify-between md:items-start md:gap-0'>
-            <div className=' max-w-xl'>
+            <div className='max-w-xl'>
                 <h1 className='text-4xl font-bold md:leading-tight md:text-5xl'>
                     Unleash Your Job Search with Our Innovative Application Tool.
                 </h1>
                 <p className='mt-7 text-sm md:text-base'>Effortlessly create and manage job postings, 
                 secure and confidential platform to protect your hiring data.</p>
-                <Link to={'/register'}>
-                    <button className='mt-7 bg-gradient-to-r from-[#711beb] to-[#b24af2] py-2 px-10 rounded-md font-semibold hover:scale-95 transition ease-out duration-500'>
-                        Register
-                    </button>
-                </Link>
-                <Link to={'/signIn'}>
-                    <button className='transition ease-out duration-500 border-2 ml-7 border-[#b135eb] py-2 px-10 rounded-md hover:bg-[#b135eb] font-semibold'>
-                        Sign In
-                    </button>
-                </Link>
+                <div className='flex flex-col justify-center items-center md:flex-row gap-4 mt-7'>
+                    <Link to={'/register'}>
+                        <button className='bg-gradient-to-r from-[#711beb] to-[#b24af2] py-2 px-10 rounded-md font-semibold hover:scale-95 transition ease-out duration-500'>
+                            Register
+                        </button>
+                    </Link>
+                    <Link to={'/signIn'}>
+                        <button className='transition ease-out duration-500 border-2 border-[#b135eb] py-2 px-10 rounded-md hover:bg-[#b135eb] font-semibold'>
+                            Sign In
+                        </button>
+                    </Link>
+                </div>
             </div>
             <div className='relative md:w-ful'>
                 <img draggable="false" src="/hero.png" alt="" className=' relative z-20 h-[320px] md:h-[370px]'/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import Logo from './Logo'
 import { Link } from 'react-router-dom'
+import {isMobileScreen} from '../utils/common.utils'
 
 function NavBar() {
+  const isMobile = isMobileScreen();
+
   return (
     <div className='text-gray-400 flex items-center mx-auto px-10 py-10 relative md:max-w-5xl md:px-0'>
         <div className='flex-grow'>
             <Logo/>
         </div>
-        <Link to={'/register'}>
-          <h3 className="cursor-pointer mr-10 hover:text-white hover:-translate-y-1 transition ease-out duration-500">Sign Up</h3>
-        </Link>
-        <Link to={'/signIn'}>
-          <h3 className='cursor-pointer hover:text-white hover:-translate-y-1 transition ease-out duration-500'>Log In</h3>
-        </Link>
+        {!isMobile && (<>
+          <Link to={'/register'}>
+            <h3 className="cursor-pointer text-sm md:text-base mx-2 md:mr-10 hover:text-white hover:-translate-y-1 transition ease-out duration-500">Sign Up</h3>
+          </Link>
+          <Link to={'/signIn'}>
+            <h3 className='cursor-pointer hover:text-white hover:-translate-y-1 transition ease-out duration-500'>Log In</h3>
+          </Link>
+        </>)}
     </div>
   )
 }
